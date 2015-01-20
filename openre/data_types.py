53198183b6b4af2b6d1e894233765097bcfc0ad8
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2015-01-20 20:07:19 +0300

    Data types

---
+++ openre/data_types.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Внутренние типы данных (могут быть разными в разных доменах).
+Могут меняться, например, для экономии памяти.
+"""
+import numpy as np
+
+class DataTypes(object):
+    def __init__(self):
+        self.threshold = np.int32
+        self.neuron_level = np.int32
+        self.neuron_flags = np.uint8
+        self.index_flags = np.uint8
+        self.sinapse_level = np.uint16
+        self.address = np.uint32
+        self.tick = np.uint32
+
+
+types = DataTypes()
+
+# Константы (могут быть разными в разных доменах):
+null = np.iinfo(types.address).max
+
+def test_data_types():
+    assert(null == np.iinfo(types.address).max)
