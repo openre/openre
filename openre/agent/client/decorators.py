47d0b01ff2da1cbacc632eafa8257bc8f32825f0
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2015-10-01 23:44:19 +0300

    Decorator itself

---
+++ openre/agent/client/decorators.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+
+def proxy_call_to_domains(f):
+    """
+    Запускает точно такую же функцию на всех доменах входящих в сеть
+    """
+    @wraps(f)
+    def wrapped(net, *args, **kwargs):
+        name = f.__name__
+        net.set_task(name, state='run')
+        try:
+            for domain in net.domains:
+                getattr(domain, name)(*args, **kwargs)
+            f(net, *args, **kwargs)
+        except Exception:
+            net.set_task(name, state='error')
+            raise
+        # wait for proper domain state from server
+        net.set_task('config', state='success')
+    return wrapped
+
+
