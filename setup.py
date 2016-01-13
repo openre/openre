589371bc0b4528b93fad2895b45d2aafb19f176b
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2016-01-13 13:05:03 +0300

    Fix module setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 DESCRIPTION = ("OpenRE - self-learning neural network")
 #LONG_DESCRIPTION = open('README.rst').read()
-VERSION = __import__('openre').__version__
+VERSION = '0.0.2'
 
 setup(
     name='openre',
@@ -16,25 +16,7 @@ setup(
 #    license=open('LICENSE').read(),
     platforms=["any"],
     packages=find_packages(),
-    install_requires=[
-        'Jinja2==2.7.3',
-        'MarkupSafe==0.23',
-        'appdirs==1.4.0',
-        'argparse==1.2.1',
-        'decorator==3.4.0',
-        'distribute==0.6.24',
-        'docutils==0.12',
-        'lockfile==0.10.2',
-        'numpy==1.7.1',
-        'py==1.4.26',
-        'pyopencl==2014.1',
-        'pytest==2.6.4',
-        'python-daemon==2.0.5',
-        'pytools==2014.3.5',
-        'pyzmq==14.6.0',
-        'six==1.9.0',
-        'wsgiref==0.1.2',
-    ],
+    install_requires=[],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Environment :: Console',
@@ -44,5 +26,14 @@ setup(
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
+    include_package_data=True,
+    scripts=[
+        'bin/openre-agent',
+        'bin/openre-broker',
+        'bin/openre-domain',
+        'bin/openre-proxy',
+        'bin/openre-server',
+        'bin/openrectl'],
+    package_data={'': ['license.txt']}
 )
 
