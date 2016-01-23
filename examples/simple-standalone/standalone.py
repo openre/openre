779b5943a0dbd8ab0dcf94a001f77eeb42628664
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2016-01-23 22:01:32 +0300

    Turn on debug logs printing for standalone example

--- examples/simple-standalone/standalone.py
+++ examples/simple-standalone/standalone.py
@@ -1,5 +1,11 @@
 from openre import OpenRE
 from openre.agent.helpers import from_json
+import logging
+logging.basicConfig(
+    format='%(levelname)s:%(message)s',
+    level='DEBUG'
+)
+
 config = None
 with open('./config.json') as json_file:
     config = from_json(json_file.read())
