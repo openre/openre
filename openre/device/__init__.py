c74ba3c03603694dacec5f1560c4e43b1e24c557
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2016-01-26 10:38:35 +0300

    Dynamically load devices

--- openre/device/__init__.py
+++ openre/device/__init__.py
@@ -1,4 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from openre.device.opencl import OpenCL
-from openre.device.dummy import Dummy
+from openre.device.abstract import Device
+import os
+import importlib
+import inspect
+
+base_dir = os.path.dirname(__file__)
+for module_file in sorted(
+    [file_name for file_name in os.listdir(base_dir) \
+     if ((os.path.isfile('%s/%s' % (base_dir, file_name)) \
+          and file_name[-3:] == '.py') \
+         or os.path.isfile('%s/%s/__init__.py' % (base_dir, file_name)))
+        and file_name not in ['__init__.py', 'abstract.py']]
+):
+    module_name = module_file.split('.')
+    if module_name[-1] == 'py':
+        del module_name[-1]
+    module_name = '.'.join(module_name)
+    module = importlib.import_module(
+        'openre.device.%s' % module_name
+    )
+    for attr_name in dir(module):
+        if attr_name[0:2] == '__' or attr_name in ['Device']:
+            continue
+        attr = getattr(module, attr_name)
+        if not inspect.isclass(attr):
+            continue
+        if issubclass(attr, Device):
+            globals().update({attr_name: attr})
