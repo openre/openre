a4b155c52b6ffe879e1fc3c4d02d89f9de1f7b6a
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2016-02-04 10:26:04 +0300

    Remove excess code

--- openre/device/numpy/__init__.py
+++
@@ -1,3 +0,0 @@
-# register devices:
-from openre.device.numpy.input import NumpyInput
-from openre.device.numpy.output import NumpyOutput
--- openre/device/numpy/output.py
+++
@@ -1,5 +0,0 @@
-from openre.device.abstract import Device
-
-class NumpyOutput(Device):
-    pass
-
