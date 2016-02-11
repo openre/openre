4a32c3d0a0435f2a2ab2cd078fe690fc8bb8123f
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2016-02-11 19:45:01 +0300

    Skip tests for devices with ImportError exception

--- conftest.py
+++ conftest.py
@@ -3,9 +3,34 @@ import os
 import importlib
 import logging
 
-base_dir = os.path.join(os.path.dirname(__file__), 'openre/device')
 collect_ignore = []
 
+def get_modules(base_dir):
+    ret = []
+    device_group_name = os.path.basename(base_dir)
+    if not os.path.isdir(base_dir):
+        return []
+    for module_file in sorted(
+        [file_name for file_name in os.listdir(base_dir) \
+         if ((os.path.isfile('%s/%s' % (base_dir, file_name)) \
+              and file_name[-3:] == '.py'))
+            and file_name not in ['__init__.py']]
+    ):
+        module_name = module_file.split('.')
+        if module_name[-1] == 'py':
+            del module_name[-1]
+        module_name = '.'.join(module_name)
+        try:
+            importlib.import_module(
+                'openre.device.%s.%s' % (device_group_name, module_name)
+            )
+        except ImportError:
+            ret.append(
+                'openre/device/%s/%s.py' % (device_group_name, module_name)
+            )
+    return ret
+
+base_dir = os.path.join(os.path.dirname(__file__), 'openre/device')
 # Ignore openre.devices with ImportError exception
 for module_file in sorted(
     [file_name for file_name in os.listdir(base_dir) \
@@ -18,6 +43,9 @@ for module_file in sorted(
     if module_name[-1] == 'py':
         del module_name[-1]
     module_name = '.'.join(module_name)
+    for row in get_modules(os.path.join(base_dir, module_file)):
+        collect_ignore.append(row)
+
     try:
         module = importlib.import_module(
             'openre.device.%s' % module_name
@@ -36,4 +64,5 @@ for module_file in sorted(
             'openre/device/%s.py' % module_name
         )
         continue
+
 
