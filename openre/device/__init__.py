7fa820b9cbaee26efd4fcd9ef35d6475e84ba53a
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2016-03-16 11:33:43 +0300

    Device register function

--- openre/device/__init__.py
+++ openre/device/__init__.py
@@ -8,34 +8,10 @@ from openre.templates import TEMPLATES_LOCATIONS
 import logging
 from openre.helpers import DEVICES
 
-base_dir = os.path.dirname(__file__)
 
-
-for module_file in sorted(
-    [file_name for file_name in os.listdir(base_dir) \
-     if ((os.path.isfile('%s/%s' % (base_dir, file_name)) \
-          and file_name[-3:] == '.py') \
-         or os.path.isfile('%s/%s/__init__.py' % (base_dir, file_name)))
-        and file_name not in ['__init__.py', 'abstract.py']]
-):
-    module_name = module_file.split('.')
-    if module_name[-1] == 'py':
-        del module_name[-1]
-    module_name = '.'.join(module_name)
-    try:
-        module = importlib.import_module(
-            'openre.device.%s' % module_name
-        )
-    except ImportError as err:
-        logging.debug(
-            'Module %s not imported, so devices from this module will not be ' \
-            'available. Error: %s',
-            'openre.device.%s' % module_name,
-            str(err)
-        )
-        DEVICES.import_error('openre.device.%s' % module_name, str(err))
-        continue
-    TEMPLATES_LOCATIONS.add('openre.device.%s' % module_name)
+def find_in_module(module):
+    module_full_name = module.__name__
+    TEMPLATES_LOCATIONS.add(module_full_name)
     devices = []
     for attr_name in dir(module):
         if attr_name[0:2] == '__' or attr_name in ['Device']:
@@ -47,5 +23,35 @@ for module_file in sorted(
             globals().update({attr_name: attr})
             devices.append(attr_name)
 
-    DEVICES.import_success('openre.device.%s' % module_name, devices)
+    if devices:
+        DEVICES.import_success(module_full_name, devices)
 
+def find_in_dir(base_dir, module_prefix=''):
+
+    for module_file in sorted(
+        [file_name for file_name in os.listdir(base_dir) \
+         if ((os.path.isfile('%s/%s' % (base_dir, file_name)) \
+              and file_name[-3:] == '.py') \
+             or os.path.isfile('%s/%s/__init__.py' % (base_dir, file_name)))
+            and file_name not in ['__init__.py', 'abstract.py']]
+    ):
+        module_name = module_file.split('.')
+        if module_name[-1] == 'py':
+            del module_name[-1]
+        module_name = '.'.join(module_name)
+        module_full_name = '%s%s' % (module_prefix, module_name)
+        try:
+            module = importlib.import_module(module_full_name)
+            find_in_module(module)
+        except ImportError as err:
+            logging.debug(
+                'Module %s not imported, so devices from this module will not' \
+                ' be available. Error: %s',
+                module_full_name,
+                str(err)
+            )
+            DEVICES.import_error(module_full_name, str(err))
+            continue
+
+
+find_in_dir(os.path.dirname(__file__), 'openre.device.')
